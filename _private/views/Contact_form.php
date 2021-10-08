e81b669fc7fe0e6b71c3791224ab84d9c6a18c12
unknown <31924@ma-web.nl>
2021-10-08 13:58:33 +0200

    Bezig met Contactpagina

---
+++ _private/views/Contact_form.php
@@ -0,0 +1,68 @@
+<?php $this->layout('layouts::website1');?>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
+
+
+
+<ul class="nav1">
+  <li class="knop"><a href="#home">Home</a></li>
+  <li class="knop"><a href="#news">Over ons</a></li>
+  
+  <li class="knop"><a href="#about">Word Transformer</a></li>
+</ul>
+
+
+   
+
+
+
+
+
+
+<form action="" method="POST">
+<main>
+<div class="container">
+		<div class="contact-box">
+			<div class="left">
+				
+			</div>
+			<div class="right">
+                
+
+                <p>Wilt u het liefst eerst contact met ons opnemen vul<br> dan het formulier hier onder in of bel ons gerust op.</p>
+				<h2>Contact</h2>
+				<input type="text" class="field" placeholder="Naam">
+				<input type="text" class="field" placeholder="Email">
+				
+				<textarea placeholder="Bericht" class="field"></textarea>
+				<button class="btn">Verstuur</button>
+			</div>
+		</div>
+	</div>
+</main>
+</form>
+<footer>
+      <div class="footer-content">
+          <h3 class="footer3">Transformers Community!</h3>
+          <p class="par3">volg ons op onze socials hieronder!</p>
+          <ul class="socials">
+            <li><a href="https://www.instagram.com/transformer.community/"><i class="fa fa-instagram"></i></a></li>
+            <li><a href="https://www.facebook.com/transformers.community"><i class="fa fa-facebook"></i></a></li>
+            <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+            <li><a href="https://www.youtube.com/channel/UCvN0_IYGVy6s1jFj2enGIsw"><i class="fa fa-youtube"></i></a></li>
+          </ul>
+      </div>
+      
+    </footer>
+    <footer class="footer2">
+      <p class="par1">&copy;2021 Transformers Community</p>
+      
+    </footer>
+
+
+
+
+
+
+
+
